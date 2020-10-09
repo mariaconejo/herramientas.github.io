964a2edc5d14e4ab9d0a66b7a66b862af651ddb9
mariaconejo <mary03081@gmail.com>
2020-10-08 19:44:59 -0600

    Se agrego proyecto modal a herramientas

---
+++ modal/js/script.js
@@ -0,0 +1,9 @@
+function open(){
+    document.getElementById('vent').style.display='block';
+    document.getElementById('overlay').style.display='block';
+}
+
+function close(){
+    document.getElementById('vent').style.display='none';
+    document.getElementById('overlay').style.display='none';
+}
